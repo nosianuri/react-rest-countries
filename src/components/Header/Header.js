96e130474896c74d9b0d820a4284ec462fc07da0
nosianuri <nurinosia@gmail.com>
2022-03-22 09:06:33 +0600

    countries faced

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-            <h2>Welcome to my React website.</h2>
+            <h1>Welcome to my React website.</h1>
             <nav className='menu'>
                 <a href="/home">Home</a>
                 <a href="/shop">Shop</a>
